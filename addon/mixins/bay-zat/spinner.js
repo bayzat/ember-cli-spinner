d5166c2f4a0ebe3d6e56b41d1a62bd95aa3be411
Michal Kechner <michal@kechner.name>
2016-04-10 19:02:14 +0200

    cleanup mixin

--- addon/mixins/bay-zat/spinner.js
+++ addon/mixins/bay-zat/spinner.js
@@ -1,32 +1,10 @@
 import Ember from 'ember';
 
-const { getOwner } = Ember;
+const { computed, getOwner } = Ember;
 const addonName = 'ember-cli-spinner';
 
 function toggleSpinner(value) {
-  try {
-    let controller = this.controllerFor('application');
-    let config = getOwner(this).resolveRegistration('config:environment');
-    let rootElement = Ember.$(config.rootElement || 'body');
-
-    const loadingClassName = config[addonName].loadingClassName || 'is-loading';
-
-    if (controller) {
-      controller.set('isLoading', value);
-    }
-
-    rootElement.toggleClass(loadingClassName, value);
-
-    if(this.router) {
-      this.router.one('didTransition', function() {
-        if (controller) {
-          controller.set('isLoading', !value);
-        }
-
-        rootElement.toggleClass(loadingClassName, !value);
-      });
-    }
-  } catch (err) {}
+  this.set('isLoading', value);
 }
 
 function showSpinner() {
@@ -38,8 +16,33 @@ function hideSpinner() {
 }
 
 export default Ember.Mixin.create({
+  isLoading: computed('controller', {
+    get(key) {
+      return this._super(...arguments);
+    },
+    set(key, value) {
+      let controller = this.controllerFor('application');
+      let config = getOwner(this).resolveRegistration('config:environment');
+      let rootElement = Ember.$(config.rootElement || 'body');
+      let loadingClassName = config[addonName].loadingClassName || 'is-loading';
+
+      controller.set('isLoading', value);
+      rootElement.toggleClass(loadingClassName, value);
+
+      if(this.router) {
+        this.router.one('didTransition', function() {
+          controller.set('isLoading', !value);
+          rootElement.toggleClass(loadingClassName, !value);
+        });
+      }
+
+      return this._super(...arguments);
+    }
+  }),
+
   actions: {
     loading: showSpinner,
+    willTransition: showSpinner,
     error: hideSpinner,
     finished: hideSpinner
   }
